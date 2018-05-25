56d763da117e7b39d8775535e66b895ecec204c8
Juan A. Ruz <juanantonioruz@gmail.com>
2018-05-25 11:28:15 +0200

    Translated current configs to LP

--- init.el
+++ init.el
@@ -1,9 +1,17 @@
+;; emacs installed following this steps
+;; https://stackoverflow.com/a/44094859/1074389
+;; brew uninstall emacs
+;; brew cleanup
+;; brew doctor
+;; brew update
+;; brew upgrade
+;; brew install emacs --with-cocoa --with-gnutls --with-rsvg --with-imagemagick
+
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")
-			 ("melpa-stable" . "https://stable.melpa.org/packages/")
-			 ))
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")))
 (package-initialize)
 
 ;; * use my orgmode 
@@ -17,6 +25,7 @@
       org-confirm-babel-evaluate nil
       org-support-shift-select 'always)
 
+;; https://harryrschwartz.com/2016/02/15/switching-to-a-literate-emacs-configuration.html
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 (custom-set-faces
@@ -30,4 +39,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit yasnippet which-key macrostep cider))))
+ '(package-selected-packages
+   (quote
+    (toc-org logview helm helm-open-github restclient win-switch paredit idomenu projectile multiple-cursors smartparens rainbow-delimiters ob-http expand-region company-flx company company-mode magit yasnippet which-key macrostep cider))))
